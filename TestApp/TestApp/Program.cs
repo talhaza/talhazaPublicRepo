9af4df93c85845d6a3e2b9b2ba67890f7ad2c3b7
talhaza <talhaza@users.noreply.github.com>
2016-10-19 10:30:37 -0700

    Test project uploaded
    just simple solution added.

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = System.Environment.MachineName;
+            Console.WriteLine(string.Format("The Machine Name is :{0}", hostName));
+            Console.ReadKey();
+        }
+    }
+}
